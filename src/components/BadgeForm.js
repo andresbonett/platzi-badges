286766763c8c20ab09175b9a2d313d0a28c6c876
Andres Bonett <andresbonettm@gmail.com>
2020-07-13 21:45:20 -0500

    clases a funcion con hook en BadgeForm

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,83 +1,80 @@
 import React from "react";
 
-class BadgeForm extends React.Component {
-  handleClick = (e) => {
+function BadgeForm(props) {
+  const handleClick = (e) => {
     console.log("Clicked");
   };
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state);
   };
 
-  render() {
-    return (
-      <React.Fragment>
-        <h1>New Attendant</h1>
+  return (
+    <React.Fragment>
+      <h1>New Attendant</h1>
 
-        <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label>First Name</label>
-            <input
-              onChange={this.props.onChange}
-              className="form-control"
-              type="text"
-              name="firstName"
-              value={this.props.formValues.firstName}
-            />
-          </div>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>First Name</label>
+          <input
+            onChange={props.onChange}
+            className="form-control"
+            type="text"
+            name="firstName"
+            value={props.formValues.firstName}
+          />
+        </div>
 
-          <div className="form-group">
-            <label>Last Name</label>
-            <input
-              onChange={this.props.onChange}
-              className="form-control"
-              type="text"
-              name="lastName"
-              value={this.props.lastname}
-            />
-          </div>
+        <div className="form-group">
+          <label>Last Name</label>
+          <input
+            onChange={props.onChange}
+            className="form-control"
+            type="text"
+            name="lastName"
+            value={props.lastname}
+          />
+        </div>
 
-          <div className="form-group">
-            <label>Email</label>
-            <input
-              onChange={this.props.onChange}
-              className="form-control"
-              type="email"
-              name="email"
-              value={this.props.email}
-            />
-          </div>
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            onChange={props.onChange}
+            className="form-control"
+            type="email"
+            name="email"
+            value={props.email}
+          />
+        </div>
 
-          <div className="form-group">
-            <label>Job Title</label>
-            <input
-              onChange={this.props.onChange}
-              className="form-control"
-              type="text"
-              name="jobTitle"
-              value={this.props.jobTitle}
-            />
-          </div>
+        <div className="form-group">
+          <label>Job Title</label>
+          <input
+            onChange={props.onChange}
+            className="form-control"
+            type="text"
+            name="jobTitle"
+            value={props.jobTitle}
+          />
+        </div>
 
-          <div className="form-group">
-            <label>twitter</label>
-            <input
-              onChange={this.props.onChange}
-              className="form-control"
-              type="text"
-              name="twitter"
-              value={this.props.twitter}
-            />
-          </div>
+        <div className="form-group">
+          <label>twitter</label>
+          <input
+            onChange={props.onChange}
+            className="form-control"
+            type="text"
+            name="twitter"
+            value={props.twitter}
+          />
+        </div>
 
-          <button onClick={this.handleClick} className="btn btn-primary">
-            Save
-          </button>
-        </form>
-      </React.Fragment>
-    );
-  }
+        <button onClick={handleClick} className="btn btn-primary">
+          Save
+        </button>
+      </form>
+    </React.Fragment>
+  );
 }
 
 export default BadgeForm;
