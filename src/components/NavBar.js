375257647c6672ed93d7b17a54533bd703cd9793
Andres Bonett <andresbonettm@gmail.com>
2020-07-13 21:47:59 -0500

    clases a funcion con hook en NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,20 +3,18 @@ import Logo from "../images/logo.svg";
 import "./styles/Navbar.css";
 import { Link } from "react-router-dom";
 
-class NavBar extends React.Component {
-  render() {
-    return (
-      <div className="Navbar">
-        <div className="container-fluid">
-          <Link className="Navbar__brand" to="/">
-            <img className="Navbar__brand-lofo" src={Logo} alt="Logo" />
-            <span className="font-weight-light">Platzi</span>
-            <span className="font-weight-dark">Conf</span>
-          </Link>
-        </div>
+function NavBar() {
+  return (
+    <div className="Navbar">
+      <div className="container-fluid">
+        <Link className="Navbar__brand" to="/">
+          <img className="Navbar__brand-lofo" src={Logo} alt="Logo" />
+          <span className="font-weight-light">Platzi</span>
+          <span className="font-weight-dark">Conf</span>
+        </Link>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default NavBar;
