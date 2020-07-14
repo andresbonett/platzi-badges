1ae74970c046f9fcb6d153695a4f71c59ef920e4
Andres Bonett <andresbonettm@gmail.com>
2020-07-13 19:29:41 -0500

    avanzando en el curso

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -2,32 +2,25 @@ import React from "react";
 import confLogo from "../images/badge-header.svg";
 import "./styles/Badge.css";
 
-class Badge extends React.Component {
-  render() {
-    return (
-      <div className="Badge">
-        <div className="Badge__header">
-          <img src={confLogo} alt="Avatar" />
-        </div>
-        <div className="Badge__section-name">
-          <img
-            className="Badge__avatar"
-            src={this.props.avatarUrl}
-            alt="avatar"
-          />
-          <h1>
-            {this.props.firstName} <br />
-            {this.props.lastName}
-          </h1>
-        </div>
-        <div className="Badge__section-info">
-          <h3>{this.props.jobTitle}</h3>
-          <div>@{this.props.twitter}</div>
-        </div>
-        <div className="Badge__footer">#PlatziConf </div>
+function Badge(props) {
+  return (
+    <div className="Badge">
+      <div className="Badge__header">
+        <img src={confLogo} alt="Avatar" />
       </div>
-    );
-  }
+      <div className="Badge__section-name">
+        <img className="Badge__avatar" src={props.avatarUrl} alt="avatar" />
+        <h1>
+          {props.firstName} <br />
+          {props.lastName}
+        </h1>
+      </div>
+      <div className="Badge__section-info">
+        <h3>{props.jobTitle}</h3>
+        <div>@{props.twitter}</div>
+      </div>
+      <div className="Badge__footer">#PlatziConf </div>
+    </div>
+  );
 }
-
 export default Badge;
--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -2,29 +2,27 @@ import React from "react";
 
 import "./styles/BadgesList.css";
 
-class BadgesList extends React.Component {
-  render() {
-    return (
-      <li className="BadgesList">
-        <div className="BadgesListItem ">
-          <img
-            className="BadgesListItem__avatar"
-            src={this.props.avatarUrl}
-            alt="avatar"
-          />
-          <div>
-            <h4>
-              <span>{this.props.firstName} </span>
-              <span>{this.props.lastName}</span>
-            </h4>
+function BadgesList(props) {
+  return (
+    <li className="BadgesList">
+      <div className="BadgesListItem ">
+        <img
+          className="BadgesListItem__avatar"
+          src={props.avatarUrl}
+          alt="avatar"
+        />
+        <div>
+          <h4>
+            <span>{props.firstName} </span>
+            <span>{props.lastName}</span>
+          </h4>
 
-            <div>{this.props.jobTitle}</div>
-            <div>@{this.props.twitter}</div>
-          </div>
+          <div>{props.jobTitle}</div>
+          <div>@{props.twitter}</div>
         </div>
-      </li>
-    );
-  }
+      </div>
+    </li>
+  );
 }
 
 export default BadgesList;
