3606b0712a2ac0410f386eb5a910f888f650ea14
Andres Bonett <andresbonettm@gmail.com>
2020-07-13 21:58:55 -0500

    clases a funcion con hook en Badges

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -1,91 +1,79 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles/Badges.css";
 import confLogo from "../images/platziconf-logo.svg";
 import BadgesList from "../components/BadgesList";
 import { Link } from "react-router-dom";
 import Loader from "../components/Loader";
 
-class Badges extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("1. constructor()");
-    this.state = {
-      loading: true,
-      error: null,
-      data: [],
-    };
-  }
+function Badges(props) {
+  console.log("1. constructor()");
 
-  componentDidMount() {
-    console.log("3. componentDidMount()");
+  const [state, setState] = useState({
+    loading: true,
+    error: null,
+    data: [],
+  });
 
-    this.fetchData("http://localhost:3001/badges");
-  }
-
-  fetchData = async (url) => {
-    this.setState({ loading: true, error: null });
+  const fetchData = async (url) => {
+    setState({ loading: true, error: null });
     try {
       const API = await fetch(url);
       const data = await API.json();
-      // console.log(data);
-      this.setState({ loading: false, data: data });
+      console.log(data);
+      setState({ loading: false, data: data });
     } catch (error) {
-      this.setState({ loading: false, error: error });
+      setState({ loading: false, error: error });
     }
   };
 
-  render() {
-    console.log("2. render()");
+  useEffect(() => {
+    fetchData("http://localhost:3001/badges");
+  }, []);
 
-    if (this.state.loading)
-      return (
-        <div className="loader">
-          <Loader />
-        </div>
-      );
-
+  if (state.loading)
     return (
-      <React.Fragment>
-        <div className="Badges">
-          <div className="Badges__hero">
-            <div className="Badges__container">
-              <img
-                className="Badges__conf-logo"
-                src={confLogo}
-                alt="ConfLogo"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="Badges__container">
-          <div className="Badges__buttons">
-            <Link to="/badge/new" className="btn btn-primary">
-              New Badge
-            </Link>
-          </div>
-          <div className="Badges__list">
-            <div className="Badges__container">
-              <ul className="list-unstyled">
-                {this.state.data.map((badge) => {
-                  return (
-                    <BadgesList
-                      key={badge.id}
-                      firstName={badge.firstName}
-                      lastName={badge.lastName}
-                      jobTitle={badge.jobTitle}
-                      twitter={badge.twitter}
-                      avatarUrl={badge.avatarUrl}
-                    />
-                  );
-                })}
-              </ul>
-            </div>
-          </div>
-        </div>
-      </React.Fragment>
+      <div className="loader">
+        <Loader />
+      </div>
     );
-  }
+
+  return (
+    <React.Fragment>
+      <div className="Badges">
+        <div className="Badges__hero">
+          <div className="Badges__container">
+            <img className="Badges__conf-logo" src={confLogo} alt="ConfLogo" />
+          </div>
+        </div>
+      </div>
+
+      <div className="Badges__container">
+        <div className="Badges__buttons">
+          <Link to="/badge/new" className="btn btn-primary">
+            New Badge
+          </Link>
+        </div>
+        <div className="Badges__list">
+          <div className="Badges__container">
+            <ul className="list-unstyled">
+              {state.data.map((badge) => {
+                return (
+                  <BadgesList
+                    key={badge.id}
+                    firstName={badge.firstName}
+                    lastName={badge.lastName}
+                    jobTitle={badge.jobTitle}
+                    twitter={badge.twitter}
+                    avatarUrl={badge.avatarUrl}
+                  />
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
 
 export default Badges;
