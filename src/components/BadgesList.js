17c3e3a4858445905d68628f19709ca45be4ba6d
Andres Bonett <andresbonettm@gmail.com>
2020-07-16 22:26:17 -0500

    agregando logica de BadgesList a pagina de Badges

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,78 +1,40 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 import "./styles/BadgesList.css";
 
 import { Link } from "react-router-dom";
-import Loader from "../components/Loader";
-import MiniLoader from "../components/MiniLoader";
-import API from "../api";
+
 import BadgesListItems from "./BadgesListItems";
 
 function BadgesList(props) {
-  const [state, setState] = useState({
-    loading: true,
-    error: null,
-    data: [],
-  });
-
-  const fetchData = async () => {
-    setState((prev) => {
-      return { ...prev, loading: true, error: null };
-    });
-    try {
-      const data = await API.badges.list();
-      setState((prev) => {
-        return { ...prev, loading: false, data: data, error: null };
-      });
-    } catch (error) {
-      setState({ loading: false, error: error });
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-
-    const intervalId = setInterval(() => fetchData(), 5000);
-
-    return () => clearInterval(intervalId);
-  }, []);
-
-  if (state.loading && state.data.length === 0) {
-    return <Loader />;
-  }
-
-  if (state.error !== null) {
-    return (
-      <div className="loader">
-        <h3>Ups! No connection established</h3>
-      </div>
-    );
-  }
-
-  if (state.data.length === 0) {
-    return (
-      <React.Fragment>
-        <div className="Badges__container loader">
-          <h3>No badges were found</h3>
-          <div className="Badges__buttons">
-            <Link to="/badges/new" className="btn btn-primary">
-              Create New Badge
-            </Link>
-          </div>
-        </div>
-      </React.Fragment>
-    );
-  }
-
   return (
-    <React.Fragment>
+    <div className="BadgesList">
+      <div className="form-group">
+        <label>Filter Badges</label>
+        <input
+          type="text"
+          className="form-control"
+          value=""
+          onChange={(e) => {
+            console.log(e.target.value);
+          }}
+        />
+      </div>
       <ul className="list-unstyled">
-        {state.data.map((badge) => {
-          return <BadgesListItems badge={badge} />;
+        {props.data.map((badge) => {
+          return (
+            <li key={badge.id} className="BadgesList">
+              <Link
+                className="text-reset text-decoration-none"
+                to={`/badges/${badge.id}`}
+              >
+                <BadgesListItems badge={badge} />
+              </Link>
+            </li>
+          );
         })}
       </ul>
-      <div>{state.loading && <MiniLoader />}</div>
-    </React.Fragment>
+    </div>
   );
 }
 
--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -1,10 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./styles/Badges.css";
 import confLogo from "../images/platziconf-logo.svg";
 import BadgesList from "../components/BadgesList";
+import Loader from "../components/Loader";
+import MiniLoader from "../components/MiniLoader";
+import API from "../api";
 
 function Badges() {
+  const [state, setState] = useState({
+    loading: true,
+    error: null,
+    data: [],
+  });
+
+  const fetchData = async () => {
+    setState((prev) => {
+      return { ...prev, loading: true, error: null };
+    });
+    try {
+      const data = await API.badges.list();
+      setState((prev) => {
+        return { ...prev, loading: false, data: data, error: null };
+      });
+    } catch (error) {
+      setState({ loading: false, error: error });
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+
+    const intervalId = setInterval(() => fetchData(), 5000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  if (state.loading && state.data.length === 0) {
+    return <Loader />;
+  }
+
+  if (state.error !== null) {
+    return (
+      <div className="loader">
+        <h3>Ups! No connection established</h3>
+      </div>
+    );
+  }
+
+  if (state.data.length === 0) {
+    return (
+      <React.Fragment>
+        <div className="Badges__container loader">
+          <h3>No badges were found</h3>
+          <div className="Badges__buttons">
+            <Link to="/badges/new" className="btn btn-primary">
+              Create New Badge
+            </Link>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+
   return (
     <React.Fragment>
       <div className="Badges">
@@ -23,7 +81,8 @@ function Badges() {
         </div>
         <div className="Badges__list">
           <div className="Badges__container">
-            <BadgesList />
+            <BadgesList data={state.data} />
+            <div>{state.loading && <MiniLoader />}</div>
           </div>
         </div>
       </div>
